1d9efc35d194e6b2e4bb52bc92b51f935eb66d9d
LEE-SUMIN <37135336+LEE-SUMIN@users.noreply.github.com>
2021-04-05 17:33:51 +0900

    soma_codetest2

---
+++ soma_2/src/soma_2/Main.java
@@ -0,0 +1,9 @@
+package soma_2;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ soma_3/src/soma_3/Main.java
@@ -0,0 +1,9 @@
+package soma_3;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
